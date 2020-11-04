637e88cfd47a3d6e109e37cb01add5998b1c4c5f
Yiran Li <yiran.li@hotmail.com>
2020-11-04 15:33:14 -0700

    Set up booking class initial

---
+++ test/Booking-test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import Booking from '../src/Booking';
+
+describe.only('Booking class', () => {
+  let booking1;
+  beforeEach(() => {
+    
+  })
+  describe('initialize', () => {
+    it('should be a function', () => {
+      expect(Booking).to.be.a('function');
+    })
+  })
+})
+
