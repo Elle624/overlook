1133050db7a4ab5e1cc13910a239b769e3c9f08b
Yiran Li <yiran.li@hotmail.com>
2020-11-04 12:18:45 -0700

    Add tests for returnTodayRevenue method

--- src/Manager.js
+++ src/Manager.js
@@ -4,7 +4,18 @@ class Manager extends User {
   constructor(id, name) {
     super(id, name);
   }
-}
 
+  returnTodayRevenue(date, bookings, rooms) {
+    return bookings.reduce((totalCost, booking) => {
+        rooms.forEach(room => {
+          if (booking.date === date && booking.roomNumber === room.number) {
+            totalCost += room.costPerNight;
+          }
+        });
+        return totalCost;
+    }, 0)
+  }
+
+}
 
 export default Manager;
--- test/Manager-test.js
+++ test/Manager-test.js
@@ -2,13 +2,61 @@ import { expect } from 'chai';
 import Manager from '../src/Manager';
 
 describe.only('Manager Class', () => {
-  let manager;
+  let manager, bookings, rooms;
   beforeEach(() => {
-    manager = new Manager(27, 'Amber')
+    manager = new Manager(27, 'Amber');
+    bookings = [
+      {
+        "userID": 24,
+        "date": "2019/09/23",
+        "roomNumber": 6
+      },
+      {
+        "userID": 7,
+        "date": "2019/09/23",
+        "roomNumber": 7
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/23",
+        "roomNumber": 7
+      }
+    ];
+    rooms = [
+      {"number":6,"costPerNight":405.58}, 
+      {"number":7,"costPerNight":207.64}
+    ];
   })
   describe('initialize', () => {
     it('should be a function', () => {
       expect(Manager).to.be.a('function');
+    })
+
+    it('should be an instance of Manager Class', () => {
+      expect(manager).to.be.an.instanceof(Manager);
+    })
+
+    it('should have a name', () => {
+      expect(manager.name).to.equal('Amber');
+    })
+  })
+  describe('method', () => {
+    it('should return today\'s total revenue', () => {
+      const result = manager.returnTodayRevenue("2019/09/23", bookings, rooms);
+      
+      expect(result).to.equal(613.22);
+    })
+
+    it('should be able to return a different day\'s total revenue', () => {
+      const result = manager.returnTodayRevenue("2019/06/23", bookings, rooms);
+      
+      expect(result).to.equal(207.64);
+    })
+
+    it('should return 0 if there\'s no booking on that day', () => {
+      const result = manager.returnTodayRevenue("2019/07/23", bookings, rooms);
+      
+      expect(result).to.equal(0);
     })
   })
 
