40f54a43d45e9d6a8152e1e52fd645a434e0e492
Yiran Li <yiran.li@hotmail.com>
2020-11-04 18:41:58 -0700

    Add displayMainpage function and helper function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
+
+const loginBtn = document.querySelector('#register-btn');
+const loginPage = document.querySelector('.login-page');
+const mainPage = document.querySelector('.main-page');
+
+loginBtn.addEventListener('click', displayMainPage);
+
+
+function updateElement(elements) {
+  elements.forEach(element => {
+    if(element.addHidden) {
+      element.section.classList.add('hidden')
+    } else {
+      element.section.classList.remove('hidden')
+    }
+  });
+}
+
+function displayMainPage() {
+  const sections = [{section: loginPage, addHidden: true}, {section: mainPage}];
+  updateElement(sections);
+}
