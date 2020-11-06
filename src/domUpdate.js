49f9189b0635230abec3bcd017f9e3cf9cc3bb68
Yiran Li <yiran.li@hotmail.com>
2020-11-05 23:14:23 -0700

    Clean up

--- src/domUpdate.js
+++ src/domUpdate.js
@@ -1,10 +1,11 @@
 const domUpdate = {
+  
   updateManagerTodayData: (section, rooms, revenue, occupation) => {
     section.innerHTML = '';
     section.innerHTML =
     `
     <h3 class="available-room">Available rooms today: ${rooms}</h3>
-    <h3 class="today-revenue">Today's total revenue: ${revenue.toFixed(2)}</h3>
+    <h3 class="today-revenue">Today's total revenue: $${revenue.toFixed(2)}</h3>
     <h3 class="today-occupation">Room Occupation rate: ${occupation*100}%</h3>
     `
   },
